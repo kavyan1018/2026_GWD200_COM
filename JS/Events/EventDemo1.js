a25af021f4949cfedc2c8a1751faff6f98ebd4f0
Kavyan Nayak <kavyannayak104@gmail.com>
2026-08-06 19:42:10 +0530

    Eevnts

---
+++ JS/Events/EventDemo1.js
@@ -0,0 +1,44 @@
+// function changecolor() {
+//     document.body.style.backgroundColor = "lightblue";
+// }
+
+
+// function changecolor() {
+//     alert("Mouse is over the button");
+// }
+
+
+// function changecolor() {
+//     alert("Mouse Left");
+// }
+
+// function changecolor() {
+//     alert("Mouse is clicked");
+// }
+
+// function changecolor() {    
+//     alert("Mouse is clicked");
+// }
+
+// function changecolor() {
+//     alert("Mouse is up clicked");
+// }           
+
+
+
+
+// Keyboard Events
+
+// function pressKey(){
+//     alert("Key is pressed");
+// }
+
+// function pressKey(){
+//     alert("Key is pressed");
+// }
+
+// function pressKey(){
+//     alert("Key is released");
+// }
+
+
